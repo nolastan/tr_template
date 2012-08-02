dd03a11f82628cc59632c5c9c62bdfc688612d22
jrichardlai <jrichardlai@gmail.com>
2012-08-01 22:14:47 -0700

    Remove extra lines.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
-require 'open-uri'
-require 'json'
-
 class User < ActiveRecord::Base
-  has_many :orders
-
   def self.find_or_create_from_auth_hash(auth_hash)
     user              = find_or_initialize_by_remote_id(auth_hash.uid)
     user.display_name = auth_hash.extra.raw_info.display_name
+    user.token        = auth_hash.credentials.token
     user.save
     user
+  end
+
+  def api_session
+    @api_session ||= Taskrabbit::Api.new(token)
   end
 end
